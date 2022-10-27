1cfff70d8b84c8fec7021ef00377299514cdc8ce
zezhao <zezhao2@illinois.edu>
2022-10-27 02:42:17 -0500

    init and solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 import femflex as flex
 
 
@@ -37,8 +39,8 @@ rhoc = 0.5
 am = 0.5 * (3 - rhoc) / (1 + rhoc)
 af = 1 / (1 + rhoc)
 gamma = 0.5 + am - af
-dt = 1e-3
-k0, k1 = 1.0, 1e2
+dt = 1e-2
+k0, k1 = 1.0e2, 1.0e2
 
 
 def qr(x):
@@ -48,10 +50,11 @@ def qr(x):
     return gp, gw
 
 
-def assemble(mesh, space, dT1, dT0, T0):
+def assemble(space, dT1, dT0, T0):
     ndof = space.num_dofs()
     R = np.zeros((ndof))
-    ncell = mesh.num_cells()
+    mesh = space.mesh()
+    ncell = space.mesh().num_cells()
     element = space._element
     for ic in range(ncell):
         basis_dof = space.cell_basis(ic)
@@ -71,10 +74,28 @@ def assemble(mesh, space, dT1, dT0, T0):
         gradTm_val = T0[dof].dot(basis_grad_val)
         + dt * af * (gamma * dT1[dof].dot(basis_grad_val)
                      + (1 - gamma) * dT0[dof].dot(basis_grad_val))
-        Rcell = np.dot(basis_val, dTm_val * gw) * detJ
-        + np.dot(basis_grad_val, k0 * gradTm_val * gw) * detJ
+        Rcell = np.dot(basis_val, dTm_val * gw) * detJ \
+            + np.dot(basis_grad_val, k0 * gradTm_val * gw) * detJ
         R[dof] += Rcell
+    # print(np.linalg.norm(R))
     return R
+
+
+def evaluate_vec(space, v, xp):
+    mesh = space.mesh()
+    x = mesh.node()
+    vp = np.zeros_like(xp)
+    for ix, point in enumerate(xp):
+        ic = np.argwhere(
+            np.squeeze((x <= point) * (point <= np.roll(x, -1))))[0]
+        ic = np.squeeze(ic)
+        xcell = mesh.cell_coordinates(ic)
+        xi = (point - xcell[0]) / (xcell[1] - xcell[0])
+        basis_dof = space.cell_basis(ic)
+        basis_val = space.element().eval(xi, 0, basis_dof)
+        dof = space.cell_dof(ic)
+        vp[ix] = v[dof].dot(basis_val)
+    return vp
 
 
 if __name__ == '__main__':
@@ -84,5 +105,23 @@ if __name__ == '__main__':
     dT1 = np.zeros((ndof,))
     dT0 = np.zeros((ndof,))
     T0 = np.zeros((ndof,))
-    assemble(mesh, space, dT1, dT0, dT0)
-    print(mesh.node())
+
+    for i in range(ndof):
+        T0[i] = np.sin(np.pi*i/(ndof-1)) * 1.0
+    T0[-1] = 0.0
+    nntime = 10
+    from scipy.optimize import root
+    for i in range(nntime):
+        print(f'i={i}, time={i*dt}')
+        dT1 *= (gamma - 1) / gamma
+        sol = root(lambda dTem: assemble(space, dTem, dT0, T0),
+                   dT1, tol=1e-6)
+        dT1[:] = sol.x
+        T0[:] += dt * (gamma * dT1 + (1 - gamma) * dT0)
+        np.savetxt(f'dat/tem{i}.txt', T0)
+    visual = True
+    if visual:
+        x = np.linspace(0, 1, 101)
+        Tp = evaluate_vec(space, T0, x)
+        plt.plot(x, Tp)
+        plt.show()
